13612abd508916c9f6b94727cb10b6147ae999fd
Joseph Orbegoso Pea <joe@trusktr.io>
2015-07-21 16:53:18 -0700

    v1.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'rocket:build-tools',
-    version: '1.0.1',
+    version: '1.0.2',
     // Brief, one-line summary of the package.
     summary: 'Helpers for build plugins.',
     // URL to the Git repository containing the source code for this package.

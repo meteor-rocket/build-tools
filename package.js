6e445c1e6d34c8a26b6c87e688a53e6f8dab162f
Joseph Orbegoso Pea <joe@trusktr.io>
2015-07-21 16:25:52 -0700

    Moved helpers from rocket:module to here.

--- package.js
+++ package.js
@@ -1,22 +1,37 @@
 Package.describe({
-  name: 'rocket:build-tools',
-  version: '1.0.0',
-  // Brief, one-line summary of the package.
-  summary: 'Helpers for build plugins.',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'rocket:build-tools',
+    version: '1.0.0',
+    // Brief, one-line summary of the package.
+    summary: 'Helpers for build plugins.',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/meteor-rocket/build-tools.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
+Npm.depends({
+    'lodash': '3.8.0',
+    'glob': '5.0.5',
+    'user-home': '1.1.1',
+    'regexr': '1.1.1',
+})
+
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('build-tools.js');
+    api.versionsFrom('1.1.0.2');
+
+    api.use([
+        'sanjo:meteor-files-helpers@1.1.0_6',
+        'package-version-parser@3.0.3',
+        'jsx'
+    ], 'server')
+
+    api.addFiles('build-tools.jsx', 'server');
+    api.export('BuildTools', 'server')
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('rocket:build-tools');
-  api.addFiles('build-tools-tests.js');
+    api.use('tinytest');
+    api.use('rocket:build-tools');
+    api.addFiles('build-tools-tests.js');
 });
